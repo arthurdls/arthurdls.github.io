7c92ffe32eba339a9dcf1eaaec27795940209a3b
Arthur De Los Santos <arthurdls04@gmail.com>
2026-02-22 21:03:00 -0500

    update the hero bg and spacing

--- next-portfolio-main/components/skills/Skills.tsx
+++ next-portfolio-main/components/skills/Skills.tsx
@@ -9,7 +9,7 @@ interface Props {
 const Skills = ({ skillData }: Props) => {
 
     return (
-        <SectionWrapper id='skills' className="min-h-screen mt-12 md:mt-0 mx-4 md:mx-0 xl:my-20 2xl:my-0">
+        <SectionWrapper id='skills' className="mt-12 md:mt-0 mx-4 md:mx-0 xl:mt-12 xl:mb-40 2xl:mt-12 2xl:mb-40">
             <h2 className="text-4xl text-center">Tech Stack</h2>
 
             <div className="lg:w-3/4 2xl:w-3/5 my-8 mx-auto md:px-12 grid grid-cols-3 md:grid-cols-4 xl:grid-cols-5 place-items-center gap-8">
